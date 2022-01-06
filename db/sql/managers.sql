8fbb4ad3f631c633d4a9ce383a2cdfdc8473034a
tursunkhuja <tursunkhuja@go.alif.academy>
2022-01-06 20:24:03 +0500

    Go26-SQL2-task2_done

--- db/sql/managers.sql
+++ db/sql/managers.sql
@@ -1,6 +1,6 @@
-SELECT m.id, m.name, m.salary*1000 salary, m.plan*1000 plan, ss.total FROM managers m
-JOIN(SELECT s.manager_id, 
+SELECT m.id, m.name, m.salary*1000 salary, m.plan*1000 plan, COALESCE(ss.total, 0) total FROM managers m
+LEFT JOIN(SELECT s.manager_id, 
     SUM((SELECT SUM(sp.price * sp.qty) FROM sale_positions sp
 WHERE s.id = sp.sale_id)) total FROM sales s GROUP BY s.manager_id
  ) ss ON m.id = ss.manager_id
- ORDER BY ss.total DESC
+ ORDER BY total DESC
